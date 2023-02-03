0a402f1e6ed67b6504bac5d494612626e37eca6d
Alejandro quiñones <agquinones@gmail.com>
2023-02-02 21:12:19 -0300

    app de perfil completa

---
+++ registration/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import Profile, User
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['avatar','bio','link']
+        widgets = {
+            'avatar' : forms.ClearableFileInput(attrs={'class':'form-control-file mt-3'}),
+            'bio' : forms.Textarea(attrs={'class':'form-control mt-3', 'rows':3, 'placeholder':'Biografia'}),
+            'link' : forms.URLInput(attrs={'class':'form-control mt-3', 'placeholder':'Enlace'}),
+        }
+
+class EmailForm(forms.ModelForm):
+    email = forms.EmailField(required=True, help_text="Requerido.")
+
+    class Meta:
+        model = User
+        fields = ['email']
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if 'email' in self.changed_data:
+            if User.objects.filter(email=email).exists():
+                raise forms.ValidationError("Email ya registrado, intenta con otro.")
+        return email
